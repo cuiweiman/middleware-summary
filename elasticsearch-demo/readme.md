bfb85ba8b702d3e3637550d2d7c114b67a308022
“cuiweiman” <cuiweiman@baidu.com>
2021-08-30 11:45:35 +0800

    elasticsearch operation

---
+++ elasticsearch-demo/readme.md
@@ -0,0 +1,12 @@
+
+[ES官方文档](https://www.elastic.co/guide/cn/elasticsearch/guide/current/getting-started.html)
+
+[ES高性能ORM库介绍：BBOSS](https://my.oschina.net/bboss/blog/1556866)
+
+[BBOSS客户端文档](https://esdoc.bbossgroups.com/)
+
+
+没什么可写的，开发时参考文档就可以了。
+
+
+
---
+++ elasticsearch-demo/src/main/java/com/middleware/es/EsApplication.java
@@ -0,0 +1,18 @@
+package com.middleware.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: cuiweiman
+ * @date: 2021/8/30 11:39
+ */
+@SpringBootApplication
+public class EsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsApplication.class, args);
+    }
+
+}
